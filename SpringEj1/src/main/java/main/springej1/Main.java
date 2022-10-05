d0cc94464ff39679a94bd4e160a3efd6299f0524
Gonzalo Cozzo <gonza007_88@hotmail.com>
2022-10-05 20:08:17 -0300

    subiendo ejercicios 1 y 3

---
+++ SpringEj1/src/main/java/main/springej1/Main.java
@@ -0,0 +1,20 @@
+
+package main.springej1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        
+        Saludo saludo = (Saludo) context.getBean("saludo");
+        String texto = saludo.imprimirSaludo();
+        System.out.println(texto);
+    }
+    
+}
---
+++ SpringEj1/src/main/java/main/springej1/Saludo.java
@@ -0,0 +1,13 @@
+
+package main.springej1;
+
+
+public class Saludo {
+    
+    public Saludo(){
+        System.out.println("Ejecutando constructor SaludoService");
+    }
+    public String imprimirSaludo(){
+        return "Hola mundo desde método Saludo!";
+    }
+}
